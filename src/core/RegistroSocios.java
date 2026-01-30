afaf8407cfd39997e95f40be41d9d797cf0e7087
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2026-01-30 12:58:05 -0400

    Create RegistroSocios.java

---
+++ src/core/RegistroSocios.java
@@ -0,0 +1,14 @@
+public class RutaApp {
+    public static void main(String[] args) {
+        System.out.println("SISTEMA RUTA-APP-PLATFORM");
+        System.out.println("Desarrollado por: System Gregory PC (RIF: 16541976-2)");
+        
+        // Estructura para los 20 socios iniciales
+        String[] socios = new String[20];
+        double costoLicencia = 15.00; // Lo que debe pagar cada uno hoy
+        
+        System.out.println("Estado: Esperando pago inicial de $15 por socio.");
+        
+        // Aquí irás registrando a los socios de la línea ejecutiva enlace
+    }
+}
